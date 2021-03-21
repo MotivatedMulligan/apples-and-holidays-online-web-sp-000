d1dd8667325f5e40718ed0f25c22a69bbfca036b
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 18:32:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,4 @@
 require 'pry'
-
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -18,31 +17,37 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  return holiday_hash.dig(:summer,:fourth_of_july,(1))
 end
-
+############################## WORKS #############
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter][:christmas] << supply
+holiday_hash[:winter][:new_years] << supply
 end
-
-
+############################## WORKS #############
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply
 end
-
+############################## WORKS #############
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
 
+  #holiday_hash.each do |season, holiday_name| season end
+    #holiday_name.each do |holiday_name, supply_array|
+ #key: hash = { } hash[:a] = 'a' hash[:a] # => 'a'.
+#holiday_hash.each_key { |season, holiday_name| season = [[key =>|, value]* ] }
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash.fetch([:winter][:christmas])
+  holiday_hash.fetch([:winter][:new_years])
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +58,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+   holiday_hash.each {|holiday_hash| holiday_hash.values}
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
 end
-
-
-
-
-
-
-
